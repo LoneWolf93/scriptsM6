1b903cbc14a9f4ade5288fe0327e0efe4376a752
LoneWolf93 <albert.esparrell@gmail.com>
2019-03-20 19:28:46 +0100

    Beautify the code. all work 100%

--- script.sh
+++ script.sh
@@ -14,33 +14,33 @@ services=(`systemctl list-unit-files|cut -d " " -f1| grep ".service"`)
 i=0
 # Recorremos un array que estan todos los servicios del sistema
 while [ $i -lt ${#services[@]} ]; do
-# Condicion de si el servicio existe en el sistema
-if [ "$service_name.service" = ${services[$i]} ]; then
-#echo ${services[$i]} "EXISTE!!"
-while $flag; do
-# Muestra el estado del servicio
-status="systemctl status $service_name"
-# Muestra si esta activado o desactivado
-current_status=`$status|grep "active"|cut -d " " -f5`
-
-# Condicion para mostrar el menu
-if [ "$current_status" = "active" ]; then
-showMenuRunning $current_status $service_name
-elif [ "$current_status" = "inactive" ]; then
-showMenuStopped $current_status $service_name
-fi
-done
-
-else
-exists=false
-fi
-#echo ${services[$i]}
-((i++))
+    # Condicion de si el servicio existe en el sistema
+    if [ "$service_name.service" = ${services[$i]} ]; then
+        #echo ${services[$i]} "EXISTE!!"
+        while $flag; do
+            # Muestra el estado del servicio
+            status="systemctl status $service_name"
+            # Muestra si esta activado o desactivado
+            current_status=`$status|grep "active"|cut -d " " -f5`
+            
+            # Condicion para mostrar el menu
+            if [ "$current_status" = "active" ]; then
+                showMenuRunning $current_status $service_name
+                elif [ "$current_status" = "inactive" ]; then
+                showMenuStopped $current_status $service_name
+            fi
+        done
+        
+    else
+        exists=false
+    fi
+    #echo ${services[$i]}
+    ((i++))
 done
 
 if [ "$exists" = "false" ]; then
-echo $service_name "no existe"
-#prueba
+    echo $service_name "no existe"
+    #prueba
 fi
 
 
