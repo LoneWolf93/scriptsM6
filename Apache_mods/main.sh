91e9158f8f93281fd45b9c413c9da5013110c20f
LoneWolf93 <albert.esparrell@gmail.com>
2019-03-20 21:20:07 +0100

    Create a new script activity mods apache

---
+++ Apache_mods/main.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function menu () {
+    echo -e "[1] Llistar moduls"
+    echo -e "[2] Activat modul"
+    echo -e "[3] Desactivar modul"
+    echo -e "[S] Sortir de l'script"
+}
+
+#mods_enabled=`ls /etc/apache2/mods-enabled/ | grep ".load" | cut -d "." -f1`
+mods_available=`ls /etc/apache2/mods-available/ | grep "*.load" | cut -d "." -f1`
+
+i=1
+for files in $mods_available; do
+    echo $files
+    
+    ((i++))
+done
