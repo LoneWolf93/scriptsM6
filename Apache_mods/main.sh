f9f88c2d337ba4f1e66fafe8831d90d24fe5affb
albert <albert@DebianDesktop>
2019-03-22 23:50:17 +0100

    Added deactivate_MOD function. All works

--- Apache_mods/main.sh
+++ Apache_mods/main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mods_enabled=`ls /etc/apache2/mods-enabled/|grep ".load"|cut -d "." -f1  2> /dev/null`
-mods_available=`ls /etc/apache2/mods-available/|grep ".load"|cut -d "." -f1  2> /dev/null`
+mods_enabled=`ls /etc/apache2/mods-enabled/|grep ".load"|cut -d "." -f1  2>/dev/null`
+mods_available=`ls /etc/apache2/mods-available/|grep ".load"|cut -d "." -f1  2>/dev/null`
 
 function menu () {
     echo -e "[1] Llistar moduls"
@@ -17,9 +17,9 @@ function list_mods () {
         local existe=`ls /etc/apache2/mods-enabled|grep ".load"|grep $file `
         
         if [ -z $existe ]; then
-            echo -e "Linea $i: \e[92m$file\e[39m" #>> disabled.txt
+            echo -e "Linea $i: \e[91m$file\e[39m" #>> disabled.txt
         else
-            echo -e "Linea $i: \e[91m$file\e[39m" #>> enabled.txt
+            echo -e "Linea $i: \e[92m$file\e[39m" #>> enabled.txt
         fi
         
         ((i++))
@@ -29,22 +29,23 @@ function list_mods () {
 function choose_option () {
     menu
     case $option in
-    1)
-        list_mods
+        1)
+            list_mods
         ;;
-    2)
-        echo "Activar MOD"
-        activate_MOD
+        2)
+            echo "Activar MOD"
+            activate_MOD
         ;;
-    3)
-        echo "Desactivar MOD"
+        3)
+            echo "Desactivar MOD"
+            deactivate_MOD
         ;;
-    [Ss])
-        echo "Bye!"
-        flag=false
+        [Ss])
+            echo "Bye!"
+            flag=false
         ;;
-    *)
-        echo "La opcion $option es desconocida"
+        *)
+            echo "La opcion $option es desconocida"
         ;;
     esac
 }
@@ -59,14 +60,38 @@ function loop_menu () {
 }
 
 function activate_MOD () {
+    local mod
     read -p "Que mod quieres activar? " mod
-
+    
     local mods_enabled=`ls /etc/apache2/mods-enabled|grep $mod.load`
-
+    
     if [ -z $mods_enabled ]; then
         echo "El mod $mod que quiere activar no esta en la lista de habilitados"
-        else
+        echo "Activando..."
+        #`cd /etc/apache2/mods-available/`
+        sleep 1
+        sudo a2enmod $mod
+        echo "Restarting apache2..."
+        sleep 1
+        `systemctl restart apache2`
+    else
         echo "Este mod $mod no puede habilitarse porque ya lo esta"
+    fi
+}
+
+function deactivate_MOD () {
+    local mod
+    read -p "Que mod quieres desactivar? " mod
+    
+    local mods_enabled=`ls /etc/apache2/mods-enabled|grep $mod.load`
+    
+    if [ -z $mods_enabled ]; then
+        echo "No se puede desactivar el modulo $mod porque no esta activado."
+    else
+        echo "Desactivando mod $mod"
+        sudo a2dismod $mod
+        `systemctl restart apache2`
+        echo "MOD: $mod ha sido desactivado."
     fi
 }
 
