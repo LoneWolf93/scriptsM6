9336e882cfde2da5626b4c16c6f2e0d10c15904c
LoneWolf93 <albert.esparrell@gmail.com>
2019-04-24 20:55:57 +0200

    funciona de momento

--- script1.sh
+++ script1.sh
@@ -4,13 +4,17 @@
 function checkService () {
     status_apache2=$(systemctl status apache2 | grep "active" | cut -d " " -f 5)
     
-    
-    
     if [ "$status_apache2" != "active" ]; then
-        echo "no esta activo"
+        echo "El servicio esta parado, avisando al administrador" | mail -s "Peligro servicio parado" albert.esparrell@gmail.com
+        for (( intentos=0; intentos<=4; intentos++ )); do
+            status_apache2=$(systemctl status apache2 | grep "active" | cut -d " " -f 5)
+            $(service apache2 restart)
+        done
+        if [ "$status_apache2" = "active" ]; then
+            echo "El servicio esta activo, todo Ok" | mail -s "Servicio activo" albert.esparrell@gmail.com
+        fi
     else
         echo "esta activo"
-        echo "Hola esto es una prueba" #| mail -s "your subject" albert.esparrell@gmail.com
     fi
     
     
