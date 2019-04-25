b26e98e7a6751a9b9d7af9fe08e7030572657499
LoneWolf93 <albert.esparrell@gmail.com>
2019-04-25 12:55:31 +0200

    problem to restart the service apache solved.

--- script1.sh
+++ script1.sh
@@ -8,13 +8,11 @@ function checkService () {
         echo "El servicio esta parado, avisando al administrador" | mail -s "Peligro servicio parado" albert.esparrell@gmail.com
         for (( intentos=0; intentos<=4; intentos++ )); do
             status_apache2=$(systemctl status apache2 | grep "active" | cut -d " " -f 5)
-            $(service apache2 restart)
+            $(systemctl restart apache2 --no-ask-password)
         done
         if [ "$status_apache2" = "active" ]; then
             echo "El servicio esta activo, todo Ok" | mail -s "Servicio activo" albert.esparrell@gmail.com
         fi
-    else
-        echo "esta activo"
     fi
     
     
