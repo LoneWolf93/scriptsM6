3557ce792272884ca8c6ec683f4c77f917bd607c
LoneWolf93 <albert.esparrell@gmail.com>
2019-04-28 12:05:52 +0200

    funciona

--- 2timelog.sh
+++ 2timelog.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-
+error_found=false
 IFS=$'\n'
 data=`LC_ALL=en_EN.utf8 date`
 data15m=`LC_ALL=en_EN.utf8 date --date '-15 min'`
@@ -12,6 +12,11 @@ for i in `grep 'error' /var/log/apache2/error.log`; do
     dlog=$(date -d "$data_log" +%s)
     #echo $dlog
     if [ $dlog -ge $data_ini ]; then
-        echo "$i"
+        echo "$i" >> prueba.log
+        error_found=true
     fi
 done
+
+if [ "$error_found" = "true" ]; then
+    echo "Hay un error en el servicio apache2, revisa el log de apache" | mail -s "Error log apache" albert.esparrell@gmail.com
+fi
--- script1.sh
+++ script1.sh
@@ -22,9 +22,12 @@ function timelog () {
 # Funcion para visualizar el estado del servicio
 function checkService () {
     status_apache2=$(systemctl status apache2 | grep "active" | cut -d " " -f 5)
+    fecha=$(date +"%d-%m-%Y")
+    hora=$(date +"%H:%M:%S")
     
     if [ "$status_apache2" != "active" ]; then
         echo "El servicio esta parado, avisando al administrador" | mail -s "Peligro servicio parado" albert.esparrell@gmail.com
+        echo "Se registró un incidente a las $hora del dia $fecha Incidente en el estado del servicio. Estado --> $status_apache2" >> prueba.log
         for (( intentos=0; intentos<=4; intentos++ )); do
             status_apache2=$(systemctl status apache2 | grep "active" | cut -d " " -f 5)
             $(systemctl restart apache2 --no-ask-password)
