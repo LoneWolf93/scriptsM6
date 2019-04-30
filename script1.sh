0bda103b4620cdbde9e7ac62267f7355d367d7c0
LoneWolf93 <albert.esparrell@gmail.com>
2019-04-30 23:48:07 +0200

    all ok

--- script1.sh
+++ script1.sh
@@ -1,24 +1,5 @@
 #! /bin/bash
 
-function timelog () {
-    IFS=$'\n'
-    data=`LC_ALL=en_EN.utf8 date`
-    data15m=`LC_ALL=en_EN.utf8 date --date '-1 min'`
-    #echo $data
-    #echo $data15m
-    data_ini=$(date -d "$data15m" +%s)
-    #echo $data_ini
-    i=$1
-    for i in `grep 'error' /var/log/apache2/error.log`; do
-        data_log=`echo $i | cut -d " " -f 1-4 | cut -c2-`
-        dlog=$(date -d "$data_log" +%s)
-        #echo $dlog
-        if [ $dlog -ge $data_ini ]; then
-            echo "$i"
-        fi
-    done
-}
-
 # Funcion para visualizar el estado del servicio
 function checkService () {
     status_apache2=$(systemctl status apache2 | grep "active" | cut -d " " -f 5)
@@ -36,14 +17,6 @@ function checkService () {
             echo "El servicio esta activo, todo Ok" | mail -s "Servicio activo" albert.esparrell@gmail.com
         fi
     fi
-
-prueba=""
-    
-    hola=`timelog $prueba`
-
-    echo $hola
-    
     
 }
 checkService
-#timelog
--- script2.sh
+++ script2.sh
@@ -1,24 +1,15 @@
 #! /bin/bash
 
-
 function monitorUse () {
     # Tanto por ciento de la partición en home
     porcentaje=$(df -h --type=ext4 --output='pcent' --output='target' | grep "/media" | cut -d " " -f2 | sed 's/.$//')
-    # Directorio donde está montado la partición
-    particiones=$(df -h --type=ext4 --output='target')
-    
-    array_porcentajes=($porcentaje)
-    #echo ${array_porcentajes[1]}
-    array_particiones=($particiones)
-    
-    longitud_porcentajes=${#array_porcentajes[@]}
-    longitud_particiones=${#array_particiones[@]}
     
     # ejercicio 2.1.2 --> ls -l /home/* --sort=size | grep ^- | head -n1
     
+    # Comprobamos si el porcentaje de la particion /home supera o es igual al 15%
     if [ $porcentaje -ge 15 ]; then
         echo "Borrando papelera"
-        # Borramos todo el contenido de la carpeta
+        # Borramos todo el contenido de la papelera
         $(rm -r /media/albert/f71fef67-1d8a-4f93-9012-94f8f8d327b3/.Trash-1000/files/* 2>/dev/null)
     fi
     
@@ -28,8 +19,18 @@ function monitorUse () {
     # Movemos los archivos de mas tamaño
     if [ $porcentaje -ge 15 ]; then
         if [ -z $papelera ]; then
-            echo "Moviendo los archivos de mas tamaño"
-            $(ls -l /home/* --sort=size | grep ^- | head -n1)
+            archivo_no_oculto=$(ls -l /home/* --sort=size | grep ^- | head -n1 | awk '{ printf $9 }' | grep ^. | cut -c 1)
+            if [ $archivo_no_oculto != . ]; then
+                usuario_del_fichero=$(ls -l /home/* --sort=size | grep ^- | head -n1 | awk '{ print $3 }')
+                if [ $usuario_del_fichero != "root" ]; then
+                    echo "Moviendo los archivos de mas tamaño"
+                    archivo=$(ls -l /home/* --sort=size | grep ^- | head -n1 | awk '{ print $9 }')
+                    $(cd /home/albert)
+                    $(mv /home/albert/$archivo /media/albert/f71fef67-1d8a-4f93-9012-94f8f8d327b3/$archivo)
+                fi
+                
+            fi
+            
         fi
     fi
     
